632b369581e002f4877d537f64c0749751c19eb4
Ankush Singh <112775365+Ash0508@users.noreply.github.com>
2024-04-21 22:15:25 +0530

    Updated Solution - GfG to GitHub

--- Easy/DFS of Graph/dfs-of-graph.java
+++ Easy/DFS of Graph/dfs-of-graph.java
@@ -38,23 +38,47 @@ class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        boolean vis[]=new boolean[V];
-        ArrayList<Integer> ans=new ArrayList<>();
-        for(int i=0;i<V;i++)
+    //     boolean vis[]=new boolean[V];
+    //     ArrayList<Integer> ans=new ArrayList<>();
+    //     for(int i=0;i<V;i++)
+    //     {
+    //         if(!vis[i])
+    //         dfs(i,adj,vis,ans);
+    //     }
+    //     return ans;
+    // }
+    // public void dfs(int v,ArrayList<ArrayList<Integer>> adj,boolean vis[],ArrayList<Integer> ans)
+    // {
+    //     vis[v]=true;
+    //     ans.add(v);
+    //     for(Integer n:adj.get(v))
+    //     {
+    //         if(!vis[n])
+    //         dfs(n,adj,vis,ans);
+    //     }
+    // }
+    boolean vis[]=new boolean[V];
+    ArrayList<Integer> ans=new ArrayList<>();
+    
+    for(int i=0;i<V;i++)
+    {
+        if(!vis[i])
         {
-            if(!vis[i])
             dfs(i,adj,vis,ans);
         }
-        return ans;
     }
-    public void dfs(int v,ArrayList<ArrayList<Integer>> adj,boolean vis[],ArrayList<Integer> ans)
+    return ans;
+}
+public void dfs(int v,ArrayList<ArrayList<Integer>> adj,boolean vis[],ArrayList<Integer> ans)
+{
+    vis[v]=true;
+    ans.add(v);
+    for(Integer n: adj.get(v))
     {
-        vis[v]=true;
-        ans.add(v);
-        for(Integer n:adj.get(v))
+        if(!vis[n])
         {
-            if(!vis[n])
             dfs(n,adj,vis,ans);
         }
     }
+}
 }

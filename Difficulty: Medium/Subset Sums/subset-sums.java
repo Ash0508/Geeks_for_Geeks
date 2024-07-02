60505e8eb5105aa3e04bbafb1747144efbbde0b8
Ankush Singh <112775365+Ash0508@users.noreply.github.com>
2024-07-02 19:07:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                arr.add(sc.nextInt());
+            }
+            Solution ob = new Solution();
+
+            ArrayList<Integer> ans = ob.subsetSums(arr, N);
+            Collections.sort(ans);
+            for (int sum : ans) {
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java//User function Template for Java
+class Solution {
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int n) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        recursion(0,n,ans,arr,0);
+        return ans;
+    }
+    void recursion(int i,int n,ArrayList<Integer> ans,ArrayList<Integer> arr,int sum)
+    {
+        if(i==n)
+        {
+            ans.add(sum);
+            return ;
+        }
+        recursion(i+1,n,ans,arr,sum+arr.get(i));
+        recursion(i+1,n,ans,arr,sum);
+    }
+}
